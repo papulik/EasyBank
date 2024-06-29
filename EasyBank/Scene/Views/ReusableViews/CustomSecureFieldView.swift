25be204c0d911242ff840f405c88e665035b9792
zuka papuashvili <zurabpapuashvili@gmail.com>
2024-06-29 21:28:00 +0400

    Add Button In SecureFields So that By Pressing it, we can see the passwords and vice versa.

--- EasyBank/Scene/Views/ReusableViews/CustomSecureFieldView.swift
+++ EasyBank/Scene/Views/ReusableViews/CustomSecureFieldView.swift
@@ -12,27 +12,72 @@ struct CustomSecureField: View {
     @Binding var text: String
     var isValid: Bool
     var validationMessage: String
-    
+    @State private var isSecure: Bool = true
+
     var body: some View {
         VStack {
-            SecureField(placeholder, text: $text)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(5.0)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5.0)
-                        .stroke(isValid ? Color.clear : Color.red, lineWidth: 1)
-                )
-                .padding(.horizontal, 20)
+            textFieldWithToggle
+            validationText
+        }
+    }
+
+    private var textFieldWithToggle: some View {
+        ZStack(alignment: .trailing) {
+            if isSecure {
+                secureField
+            } else {
+                textField
+            }
+            toggleButton
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 10)
+    }
+
+    private var secureField: some View {
+        SecureField(placeholder, text: $text)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(5.0)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5.0)
+                    .stroke(isValid ? Color.clear : Color.red, lineWidth: 1)
+            )
+    }
+
+    private var textField: some View {
+        TextField(placeholder, text: $text)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(5.0)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5.0)
+                    .stroke(isValid ? Color.clear : Color.red, lineWidth: 1)
+            )
+    }
+
+    private var toggleButton: some View {
+        Button(action: {
+            isSecure.toggle()
+        }) {
+            Image(systemName: self.isSecure ? "eye.slash.fill" : "eye.fill")
+                .foregroundColor(.gray)
+                .padding(.trailing, 30)
                 .padding(.top, 10)
-            
-            if !isValid {
+        }
+    }
+
+    private var validationText: some View {
+        if !isValid {
+            return AnyView(
                 Text(validationMessage)
                     .foregroundColor(.red)
                     .font(.caption)
                     .padding(.leading, 20)
                     .frame(maxWidth: .infinity, alignment: .leading)
-            }
+            )
+        } else {
+            return AnyView(EmptyView())
         }
     }
 }
